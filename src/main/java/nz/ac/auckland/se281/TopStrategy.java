693b86a01165dea9a6e36115ee9c90762d031ae0
minghan36 <mhua948@aucklanduni.ac.nz>
2023-05-21 02:58:01 +1200

    implemented calculations for TopStrategy

--- src/main/java/nz/ac/auckland/se281/TopStrategy.java
+++ src/main/java/nz/ac/auckland/se281/TopStrategy.java
@@ -13,6 +13,20 @@ public class TopStrategy implements Strategies{
     public int chooseSum(int fingers, Player currentPlayer) {
         List<Integer> history = currentPlayer.getFingersHistory();
         history.remove(history.size()-1);
-    }
-    
+        int mostPlayed = 0;
+        int maxCount = 0;
+        int count;
+        for (int i=1; i<=5; i++){
+            count = 0;
+            for (int finger:history){
+                if(i==finger){
+                    count++;
+                    if(count>maxCount){
+                        mostPlayed = i;
+                    }
+                }
+            }
+        }
+        return fingers + mostPlayed;
+    } 
 }
