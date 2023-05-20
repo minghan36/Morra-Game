e7be23b07d519d20b69ae3f41135918d4f1a3459
minghan36 <mhua948@aucklanduni.ac.nz>
2023-05-21 00:35:48 +1200

    Implementing Strategy design pattern

---
+++ src/main/java/nz/ac/auckland/se281/RandomStrategy.java
@@ -0,0 +1,17 @@
+package nz.ac.auckland.se281;
+
+public class RandomStrategy implements Strategies{
+
+    @Override
+    public int chooseFingers() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'chooseFingers'");
+    }
+
+    @Override
+    public int chooseSum() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'chooseSum'");
+    }
+
+}
