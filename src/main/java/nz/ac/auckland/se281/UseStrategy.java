852dd854710862add02e3edc9c633fcb1650359a
minghan36 <mhua948@aucklanduni.ac.nz>
2023-05-21 00:55:16 +1200

    created UseStrategy class for implementing the strategy design pattern

---
+++ src/main/java/nz/ac/auckland/se281/UseStrategy.java
@@ -0,0 +1,16 @@
+package nz.ac.auckland.se281;
+
+public class UseStrategy {
+    
+    private Strategies strategy;
+
+    public UseStrategy(Strategies strategy){
+        this.strategy = strategy;
+    }
+
+    public void setStrategy(Strategies strategy) {
+        this.strategy = strategy;
+    }
+
+
+}
